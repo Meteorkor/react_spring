da3b89b7822561370f1abc3e35314feaa804db3c
unseok_kim <unseok_kim@tmax.co.kr>
2016-01-15 20:25:18 +0900

    refresh 추가
    state 좋네..
    
    click은 bind(this) 해야.. 의도대로.. this가 동작..

--- react_spring/src/main/resources/static/app.js
+++ react_spring/src/main/resources/static/app.js
@@ -2,38 +2,53 @@
 const React = require('react');
 const client = require('./client');
 
+function reloadEmployees(model){
+	client({method: 'GET', path: '/api/employees'}).done(response => {
+		model.setState({employees: response.entity._embedded.employees});
+	});
+}
+
 class App extends React.Component{
+	initEmployee;
+	
 	constructor(props) {
 		super(props);
-		this.state = {employees: [] };
+		this.state = {employees: [], tok : "Now" };
+
+		
 	}
 	componentDidMount() {
-		client({method: 'GET', path: '/api/employees'}).done(response => {
-			this.setState({employees: response.entity._embedded.employees});
-		});
+		console.log("componentDidMount : ") ;
+		reloadEmployees(this);
+		
 	}
 	
 	render() {
 		return (
-				<EmployeeList employees={this.state.employees}/>
+				
+				<div>
+				
+					<EmployeeList tok={this.state.tok} employees={this.state.employees}/>
+					<input type="button" value="Refresh" onClick={this.componentDidMount.bind(this) }  />
+			</div>
 		)
 	}
 }	
 class EmployeeList extends React.Component{
+	
 	render() {
+		var tok = this.props.tok;
 		var employees = this.props.employees.map(employee =>
-			<Employee key={employee._links.self.href} employee={employee}/>
+			<Employee tok={tok} key={employee._links.self.href} employee={employee}/>
 		);
 		return (
-				<table>
-					<tr>
-						<th>Name</th>
-						<th>Desc</th>
-					</tr>
-					{employees}
-				</table>
-				
-		
+					<table>
+						<tr>
+							<th>Name</th>
+							<th>Desc</th>
+						</tr>
+						{employees}
+					</table>
 		)
 		
 		
@@ -41,16 +56,47 @@ class EmployeeList extends React.Component{
 }	
 	
 class Employee extends React.Component{
+	employeeClick(){
+	}
+	
 	render() {
 		return(
 				<tr>
-					<td>{this.props.employee.name}</td>
-					<td>{this.props.employee.description}</td>
+				{/*<td onClick={this.employeeClick}>{this.props.employee.name}</td>*/}
+					<EmployeeName employeeName={this.props.employee.name} />
+					<td>{this.props.employee.description} {this.props.tok}</td>
+					
 				</tr>
 				
 		)
 	}
 }
+
+class EmployeeName extends React.Component{
+	
+	render() {
+		var employeeName = this.props.employeeName;
+		
+		var type1 = (
+					<td>{employeeName} 11</td>
+				);
+		var type2 = (
+				<td color="red">{employeeName} 22</td>
+		);
+		
+		var nowType;
+		if(employeeName=="kim"){
+			console.log("kim Y");
+			nowType = type1;	
+		}else{
+			console.log("kim N");
+			nowType = type2;
+		}
+		
+		return nowType;
+	}
+}
+
 
 React.render(
 	<App />,
